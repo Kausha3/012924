154080f97105e489c1fc488c92c70a843884f566
Kausha3 <133591404+Kausha3@users.noreply.github.com>
2024-02-02 14:13:18 -0500

    Update logger.js

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,12 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+const errorObject = {
+  errMsg: "An error occured while processing your request."
+};
+
+const anotherObject = {
+  message: "This is a regular message."
+};
+
+logMsg(errorObject);
+logMsg(anotherObject);
