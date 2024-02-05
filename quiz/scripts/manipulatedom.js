a61bf15f695e24812fb629cfffcfb11439d964a0
Kausha3 <133591404+Kausha3@users.noreply.github.com>
2024-02-05 18:05:55 -0500

    Update manipulatedom.js

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,24 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createEditButton(row, col) {
+  let btn = document.createElement("button");
+  btn.innerHTML = "Edit text";
+  btn.onclick = function() {
+    let newText = prompt("Enter new text for Cell (" + row + ", " + col + ")", "");
+    if (newText !== null && newText !== "") {
+      this.parentNode.previousSibling.textContent = "Cell (" + row + ", " + col + ") " + newText;
+    }
+  };
+  return btn;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createEditButton(i, 0));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
